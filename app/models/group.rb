10e5abdba1490f286a9b97f5e118738262f9962c
koki-div <64241996+koki-div@users.noreply.github.com>
2020-05-24 20:44:26 +0900

    sending_message_completed

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,20 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users             # group.users によってデータを呼び出すことができるようになる
+  has_many :messages
   validates :name, presence: true, uniqueness: true  # null制約 & 一意性制約
   # has_many :messages
+
+# サイドバーの グループ 部分に最新のメッセージが表示されるようにする
+  def show_last_message
+    if (last_message = messages.last).present?    # もしもメッセージ(もしくは画像)が投稿されている存在する場合は
+      if last_message.content?                    # もしも文章が投稿されている場合は
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
